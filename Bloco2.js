e9dbe68c55c3e60b14bf197059c201f1f15458c9
ViniKnuivers <vini.knui@gmail.com>
2026-06-08 14:47:21 -0300

    Bloco2.js
    Segunda parte das atividades

---
+++ Bloco2.js
@@ -0,0 +1,66 @@
+//1
+const numeros = [1, 6, 12, 18, 60];
+console.log(numeros);
+const soma = numeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+console.log(soma);
+
+//2
+const numeros = [1, 6, 12, 18, 60];
+const maior = Math.max(...numeros);
+console.log(maior);
+
+//3
+const numeros = [1, 6, 12, 18, 60];
+const menor = Math.min(...numeros);
+console.log(menor);
+
+//4
+const numeros = [1, 1, 12, 18, 60];
+const totalnumero = {};
+
+    for (const numero of numeros){
+        if (totalnumero[numero]){
+            totalnumero[numero]++;
+        } else{
+            totalnumero[numero] = 1;
+        }
+    }
+console.log(totalnumero)
+
+//5
+const numeros = [1, 6, 12, 16, 60, 'a']
+console.log(numeros.includes(1));
+console.log(numeros.includes(5));
+console.log(numeros.includes('a'));
+
+//6
+const numeros = [1, 6, 12, 16, 60, 'a']
+console.log(numeros.indexOf(1));
+console.log(numeros.indexOf(5));
+console.log(numeros.indexOf('a'));
+
+//7
+const numeros = [1, 6, 12, 16, 60, 'a']
+console.log(numeros);
+numeros.reverse();
+console.log(numeros);
+numeros.reverse();
+console.log(numeros);
+
+//8
+const numeros = [12, 18, 1, 6, 60]
+console.log(numeros);
+numeros.sort((a, b) => a - b);
+console.log(numeros);
+
+//9
+const nome = ['Gabriela', 'Vinícius', 'Marcelo', 'Luciane', 'Gustavo'];
+console.log(nome);
+nome.sort();
+console.log(nome);
+
+//10
+const nome = ['Gabriela', 'Vinícius', 'Marcelo', 'Luciane', 'Gustavo'];
+console.log(nome);
+const nomes = nome.slice();
+console.log(nomes);
